0b76ecb1d6825f0605c81e9bc588b77f4674946a
Sivuyile Magutywa <211208086@mycput.ac.za>
2016-03-05 00:57:34 -0800

    first commit

---
+++ src/main/java/co/za/applicafro/sivuyile/App.java
@@ -0,0 +1,17 @@
+package co.za.applicafro.sivuyile;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( add(1,3) );
+    }
+
+    public static int add(int a, int b){
+        return a+b;
+    }
+}
